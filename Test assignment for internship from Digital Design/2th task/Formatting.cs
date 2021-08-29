cb2ab005eb7a7ab3455a9d45b0ef4e03db26ec68
mtokar3v <mtokar3v@gmail.com>
2021-08-30 01:28:35 +0400

    complete second task

---
+++ Test assignment for internship from Digital Design/2th task/Formatting.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2th_task
+{
+    interface IFormatting
+    {
+        public void Check(ref string text, ref int numerator);
+    }
+    class DelHtml : IFormatting
+    {
+        private int start;
+        private int length;
+        private bool progressing;
+        
+        public DelHtml()
+        {
+            start = 0;
+            length = 0;
+            progressing = false;
+        }
+
+        public void Check(ref string text, ref int numerator)
+        {
+            if (!progressing && text[numerator] == '<')
+            {
+                start = numerator;
+                progressing = true;
+            }
+
+            if (progressing && text[numerator] == '>')
+            {
+                length = numerator - start + 1;
+                text = text.Remove(start, length);
+                numerator -= length;
+                progressing = false;
+            }
+        }
+    }
+
+    class DelDash : IFormatting
+    {
+        public void Check(ref string text, ref int numerator)
+        {
+            try
+            {
+                if (text[numerator] == '-' && (text[numerator - 1] == ' ' || text[numerator + 1] == ' '))
+                {
+                    text = text.Remove(numerator, 1);
+                    numerator--;
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+
+}
--- Test assignment for internship from Digital Design/2th task/Program.cs
+++ Test assignment for internship from Digital Design/2th task/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
 
 namespace _2th_task
 {
@@ -6,18 +10,62 @@ namespace _2th_task
     {
         static void Main(string[] args)
         {
-            PrefixTree tree = new PrefixTree();
+            string path = string.Empty;
+            Console.Write("Введите путь до файла: ");
+            path = Console.ReadLine();
+            Dictionary<string, int> wordInfo = new Dictionary<string, int>();
+            
             try
             {
-                tree.Add("АбВ");
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
+                using (StreamReader stream = new StreamReader(path))
+                {
+                    string text = stream.ReadToEnd();
 
-            Console.WriteLine(tree.Find("абв"));
-            Console.ReadKey();
+                    //форматирование
+                    if (path.EndsWith(".fb2"))
+                    {
+                        IFormatting[] formattings = new IFormatting[] { new DelHtml(), new DelDash() };
+
+                        for (int i = 0; i < text.Length; i++)
+                            foreach (var format in formattings)
+                                format.Check(ref text, ref i);
+                    }
+
+                    string[] words = text.Split(new char[] { '.', '!', '?', ',', ' ','\n', '\t', '\r','/', '(', ')', ':', ';', '\"'});
+                    foreach (var t in words)
+                    {
+                        string word = t.ToLower();
+                        if (wordInfo.ContainsKey(word))
+                            wordInfo[word]++;
+                        else
+                            wordInfo.Add(word, 1);
+                    }
+
+                    if(wordInfo.ContainsKey(""))
+                        wordInfo.Remove("");
+                }
+
+                Console.Write("Введите путь для сохранения результатов: ");
+                path = Console.ReadLine() + "\\result.txt";
+
+                FileInfo fileInfo = new FileInfo(path);
+                if (!fileInfo.Exists)
+                    fileInfo.Create();
+
+                using (StreamWriter stream2 = new StreamWriter(path, false))
+                {
+                    IEnumerable<KeyValuePair<string, int>> sortWordsInfo = wordInfo.OrderByDescending(k => k.Value);
+                    stream2.WriteLine("Слово\t-\tКоличество повторений");
+                    foreach (var d in sortWordsInfo)
+                            stream2.WriteLine(d.Key + "\t-\t" + d.Value);
+                }
+                Console.WriteLine("Готово");
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
